3dca4ec89637ec4d5133a06672fb6221f0cfd09d
satomi.watanabe <satomi.watanabe@techacademy.jp>
2020-06-19 08:17:52 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,15 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show,:edit,:update,:destroy]
     
     def index
         @tasks=Task.all
     end
     
     def show
-        @task=Task.find(params[:id])
-        
     end
     
     def new
         @task=Task.new
-        
     end
     
     def create
@@ -27,12 +25,9 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task=Task.find(params[:id])
     end
     
     def update
-       @task=Task.find(params[:id]) 
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -43,7 +38,6 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
 
         flash[:success] = 'Task は正常に削除されました'
@@ -54,9 +48,11 @@ end
 
 
 private
-
+    def set_task
+        @task = Task.find(params[:id])
+    end
   # Strong Parameter
     def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
     end
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_080949) do
+ActiveRecord::Schema.define(version: 2020_06_19_075822) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
